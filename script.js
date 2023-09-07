f6d8a284c2ff3d70825e4ef074e2bd5274c856f2
kapil <khatrikapil987@gmail.com>
2023-09-07 09:14:09 +0530

    react Basics

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("working");
+import React from "react";
+import {createRoot} from "react-dom/client"
+
+const cont = React.createElement("div",{},"hello world")
+
+const root = createRoot(document.getElementById('root'));
+root.render(cont);
